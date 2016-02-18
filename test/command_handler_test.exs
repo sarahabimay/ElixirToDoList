4a1a8ac49f44679d8ba27d16026a2e8759180dec
sarahabimay <sarahabimay@gmail.com>
2016-02-18 17:31:42 +0000

    Get command from command prompt. CommandHandler to delegate commands
    * CommandHandler (prob should be CommandDelegator or CommandAgent?)
    * CommandHandler will delegate to relevant Command e.g. AddCommand
    
    Issues I'm not sure how to fix yet:
    CommandHandler has hardcoded Command options e.g. A, B, C...
    
    Next:
    ToDoTaskManager module which will tie the TasksList, the  ConsoleDisplay and
    CommandHandler/Agent together (That's how it works in my mind at the mo'
    anyways)

---
+++ test/command_handler_test.exs
@@ -0,0 +1,25 @@
+defmodule CommandHandlerTest do
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+  import ToDo.CommandHandler
+  doctest ToDo
+
+  test "valid 'add to-do task' command" do
+    add_command = "a"
+    new_task = "Buy present for Granny's birthday."
+
+    result = capture_io([input: new_task], fn ->
+      input = action_command(add_command)
+      IO.write input
+    end)
+    assert result == "New To-Do Task: > #{new_task}"
+  end
+
+  test "ignores unrecognized command" do
+    invalid_command = "9"
+    result = capture_io(fn ->
+      input = action_command(invalid_command)
+    end)
+    assert result == "Sorry, I don't recognize that command.\n"
+  end
+end
--- test/console_display_test.exs
+++ test/console_display_test.exs
@@ -20,12 +20,21 @@ defmodule ConsoleDisplayTest do
     assert String.contains?(result, "Commands Cheatsheet")
   end
 
-  test "display To-Do list with item" do
-    todo_item = "Go to yoga tonight"
+  test "display To-Do list with task" do
+    todo_task = "Go to yoga tonight"
     result = capture_io fn ->
-      display_to_do_list_and_cheatsheet([todo_item])
+      display_to_do_list_and_cheatsheet([todo_task])
     end
 
-    assert String.contains?(result, "[1] #{todo_item}\n")
+    assert String.contains?(result, "[1] #{todo_task}\n")
+  end
+
+  test "display receives input from prompt" do
+    add_command = "a"
+    result = capture_io([input: add_command, capture_prompt: false], fn ->
+      input = prompt_for_command
+      IO.write input
+    end)
+    assert result == "a"
   end
 end
